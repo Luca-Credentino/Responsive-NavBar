274843e1246a4f4419a0f9701fd14bd4fc8018fd
Luca <lucacredentino98@gmail.com>
2021-08-27 15:56:46 +0200

    Responsive NavBar (Animated) v1.0

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const menuBtn = document.querySelector(".menu-btn");
 const dropDownMenu = document.querySelector(".responsive-navbar");
+const cocaColaLogo = document.querySelector(".cocacola-logo");
+const hamburgerMenuText = document.querySelectorAll(".responsiveText");
 let openMenu = false;
 
 menuBtn.addEventListener("click", () => {
     if(!openMenu){
         menuBtn.classList.add("open");
         dropDownMenu.classList.add("dropdown");
+        hamburgerMenuText.forEach(texts => {
+            texts.classList.add("animatedText")
+        })
         openMenu = true;
     }else{
         menuBtn.classList.remove("open");
         dropDownMenu.classList.remove("dropdown");
+        hamburgerMenuText.forEach(texts => {
+            texts.classList.remove("animatedText")
+        })
         openMenu = false;
     }
 });
+
+cocaColaLogo.addEventListener("click", () => {
+    location.href = "https://www.coca-colaitalia.it/";
+});
+
